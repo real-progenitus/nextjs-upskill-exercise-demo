b4292a1719e209b843b8b57e06a7d89423d35645
pedrobernardo94 <pedro.bernardo@mindera.com>
2023-01-12 16:20:09 +0000

    full fledged app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import HeaderNav from "../components/HeaderNav";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeaderNav />
+      <Component {...pageProps} />
+    </>
+  );
 }
